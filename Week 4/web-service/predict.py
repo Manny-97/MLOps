be714df613d2130bcb227c162edbe5d284c55f65
Muhammed <muhammed@sendme.ng>
2022-08-11 00:59:31 +0100

    feat: set predict.py & test.py

---
+++ Week 4/web-service/predict.py
@@ -0,0 +1,15 @@
+import pickle
+from flask import Flask, request, jsonify
+with open("lin_reg.bin", "rb") as f_in:
+    (dv, model) = pickle.load(f_in)
+
+def prepare_features(ride):
+    features = {}
+    features['PUL_DO'] = '%s_%s' %(ride['PULocationID'], ride['DOLocationID'])
+    features['trip_distance'] = ride['trip_distance']
+    return features
+
+def predict(features):
+    X = dv.transform(features)
+    preds = model.predict(X)
+    return preds
