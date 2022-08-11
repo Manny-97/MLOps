cc7747a82fe86731baec0fb45728197a1036d7c5
Muhammed <muhammed@sendme.ng>
2022-08-11 12:01:24 +0100

    feat: setup docker image

--- Week 4/web-service/test.py
+++ Week 4/web-service/test.py
@@ -1,10 +1,11 @@
-import predict
+import requests
 
 ride = {
     "PULocationID": 10,
     "DOLocationID": 50,
     "trip_distance": 40
 }
-features = predict.prepare_features(ride)
-pred = predict.predict(features)
-print(pred)
+url = 'http://127.0.0.1:9696/predict'
+response = requests.post(url=url, json=ride)
+
+print(response.json())
